6095482bf94f515829a1c18e1765bd3cf6bbf3d3
Azura <MercuryLZ@outlook.com>
2023-07-28 15:04:18 +0800

    practice

---
+++ Practice/Practice/Practice.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+//int main() {
+//    int n = 0;
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        for (int i = 0; i < n; i++)
+//        {
+//            for (int j = 0; j < n; j++)
+//            {
+//                if (j == i|| j == n - i-1)
+//                {
+//                    printf("*");
+//               }
+//                else
+//                {
+//                    printf(" ");
+//                }
+//              
+//            }
+//            printf("\n");
+//        }
+//    }
+//
+void main(void) {
+    int a =b  c = 10;
+    a = b = c = 50;
+    printf("%d %d %D" a, b, c);
+}}
